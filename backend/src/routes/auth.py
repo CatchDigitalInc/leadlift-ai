67f45723ce0ed5c95945bed437a3c51027bb92d0
CatchDigitalInc <94125555+CatchDigitalInc@users.noreply.github.com>
2025-06-06 22:47:32 -0600

    Fix import path in auth.py

--- backend/src/routes/auth.py
+++ backend/src/routes/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request, jsonify, session
-from src.models.user import db, User
+from models.user import db, User
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import re
